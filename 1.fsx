f2886c8df7c2521c191a134d51038bd888443979
Isaac Abraham <isaac.abraham@gmail.com>
2023-12-01 12:01:12 +0000

    Day On

---
+++ 1.fsx
@@ -0,0 +1,69 @@
+#load "common.fsx"
+open System
+
+// Part 1
+let processLine (line: string) =
+    let digits = line |> Seq.filter Char.IsDigit |> Seq.map Char.GetNumericValue
+    $"{Seq.head digits}{Seq.last digits}" |> Convert.ToInt32
+
+let sample =
+    """1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet"""
+
+sample.ByNewLine() |> Seq.sumBy processLine
+
+Files[1] |> Seq.sumBy processLine
+
+// Part 2
+let words =
+    [ "one"; "two"; "three"; "four"; "five"; "six"; "seven"; "eight"; "nine" ]
+    |> List.indexed
+    |> List.map (fun (index, word) -> {| Text = word; Value = index + 1 |})
+
+let processLineSmart (line: string) =
+    let digits =
+        line
+        |> Seq.indexed
+        |> Seq.filter (snd >> Char.IsDigit)
+        |> Seq.map (fun (pos, c) ->
+            {| Position = pos
+               Value = Char.GetNumericValue c |> int |})
+        |> Seq.toList
+
+    let words =
+        words
+        |> List.collect (fun word ->
+            let matchingWords =
+                [ {| Value = word.Value
+                     Position = line.IndexOf word.Text |}
+                  {| Value = word.Value
+                     Position = line.LastIndexOf word.Text |} ]
+                |> List.filter (fun w -> w.Position > -1)
+                |> List.sortBy (fun w -> w.Position)
+
+            match matchingWords with
+            | [] -> []
+            | matchingWords -> [ List.head matchingWords; List.last matchingWords ])
+        |> List.distinct
+
+    let combined =
+        digits @ words
+        |> List.sortBy (fun r -> r.Position)
+        |> List.map (fun r -> r.Value)
+
+    $"{List.head combined}{List.last combined}" |> Convert.ToInt32
+
+let sampleHard =
+    """two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen"""
+
+sampleHard.ByNewLine() |> Seq.sumBy processLineSmart
+
+Files[1] |> Seq.sumBy processLineSmart
--- common.fsx
+++ common.fsx
@@ -4,17 +4,20 @@ module Common
 open System
 open type System.Environment
 
-/// This reusable function takes a multiline string and groups up based on whenever an empty line occurs.
-let groupByLines (data: string) =
-    data.Split([| NewLine + NewLine |], StringSplitOptions.RemoveEmptyEntries)
-    |> Array.map (fun group ->
-        group.Split([| NewLine |], StringSplitOptions.RemoveEmptyEntries)
-        |> Array.toList)
-    |> Array.toList
+type String with
+
+    member this.ByNewLine() =
+        this.Split([| NewLine; "\n" |], StringSplitOptions.RemoveEmptyEntries)
+
+    /// This reusable function takes a multiline string and groups up based on whenever an empty line occurs.
+    member this.GroupByEmptyLine() =
+        this.Split([| NewLine + NewLine; "\n" + "\n" |], StringSplitOptions.RemoveEmptyEntries)
+        |> Array.map (fun group -> group.ByNewLine() |> Array.toList)
+        |> Array.toList
 
 type Files() =
     member _.Item
-        with get file = $"data/{file}.txt"
+        with get file = System.IO.File.ReadLines $"data/{file}.txt"
 
 /// Provides access to data files using an indexer e.g. Files.[1] gets the path to the Day One data file.
 let Files = Files()
