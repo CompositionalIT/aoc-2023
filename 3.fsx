b6b69a420c55d0b22886ef51f54e69afad85e4df
Isaac Abraham <isaac.abraham@gmail.com>
2023-12-04 01:28:18 +0000

    Refactors

--- 3.fsx
+++ 3.fsx
@@ -5,66 +5,51 @@ open System.Text.RegularExpressions
 
 type NumberPosition = { StartX: int; EndX: int; Y: int }
 type SymbolPosition = { X: int; Y: int }
+type Number = { Value: int; Position: NumberPosition }
 
 type Symbol = {
     Value: char
     Position: SymbolPosition
 }
 
-type Number = { Value: int; Position: NumberPosition }
-
-type Element =
-    | Number of Number
-    | Symbol of Symbol
-
 let parse rows =
     rows
     |> Array.indexed
     |> Array.collect (fun (index, line) ->
         Regex.Matches(line, "\d+|[^.]")
-        |> Seq.map (fun r ->
-            match Int32.TryParse r.Value with
-            | true, number ->
-                Number {
-                    Value = number
+        |> Seq.map (fun m -> {|
+            Y = index
+            X = m.Index
+            Value = m.Value
+        |})
+        |> Seq.toArray)
+    |> Array.toList
+    |> List.partitionMap (fun r ->
+        match Int32.TryParse r.Value with
+        | true, number ->
+            Choice1Of2(
+                {
+                    Number.Value = number
                     Position = {
-                        Y = index
-                        StartX = r.Index
-                        EndX = r.Index + r.Length - 1
+                        Y = r.Y
+                        StartX = r.X
+                        EndX = r.X + r.Value.Length - 1
                     }
                 }
-            | false, _ ->
-                Symbol {
-                    Value = r.Value[0]
-                    Position = { X = r.Index; Y = index }
-                })
-        |> Seq.toArray)
-
-// Part 1
+            )
+        | false, _ ->
+            Choice2Of2(
+                {
+                    Symbol.Value = r.Value[0]
+                    Position = { X = r.X; Y = r.Y }
+                }
+            ))
 
 let isAdjacentTo n s =
     let left = abs (n.StartX - s.X) <= 1
     let right = abs (n.EndX - s.X) <= 1
     let Y = abs (n.Y - s.Y) <= 1
     (left || right) && Y
-
-let split elements =
-    elements
-    |> Seq.toList
-    |> List.partitionMap (function
-        | Number x -> Choice1Of2 x
-        | Symbol y -> Choice2Of2 y)
-
-let calculatePartNumbersTotal elements =
-    let numbers, symbols = split elements
-
-    numbers
-    |> List.choose (fun n ->
-        let isPartNumber =
-            symbols |> List.exists (fun s -> s.Position |> isAdjacentTo n.Position)
-
-        if isPartNumber then Some n else None)
-    |> List.sumBy _.Value
 
 let sample =
     """467..114..
@@ -79,27 +64,37 @@ let sample =
 .664.598.."""
 
 let parsedSample = sample.ByNewLine() |> parse
-let file = Files[3] |> parse
+let parsedFile = Files[3] |> parse
 
-file |> calculatePartNumbersTotal
+// Part 1
+let calculatePartNumbersTotal (numbers: Number list, symbols) =
+    numbers
+    |> List.choose (fun n ->
+        let isPartNumber =
+            symbols |> List.exists (fun s -> s.Position |> isAdjacentTo n.Position)
+
+        if isPartNumber then Some n else None)
+    |> List.sumBy _.Value
+
+parsedSample |> calculatePartNumbersTotal
+parsedFile |> calculatePartNumbersTotal
 
 // Part 2
-let (|Adjacent|) (numbers: Number list) (position: SymbolPosition) =
+let (|AdjacentTo|) (numbers: Number list) position =
     let adjacents =
         numbers |> List.filter (fun n -> position |> isAdjacentTo n.Position)
 
-    Adjacent adjacents
+    AdjacentTo adjacents
 
-let calculateGearRatio elements =
-    let numbers, symbols = split elements
-
+let calculateGearRatio (numbers, symbols) =
     symbols
     |> List.sumBy (fun symbol ->
         match symbol with
         | {
               Value = '*'
-              Position = Adjacent numbers [ a; b ]
+              Position = AdjacentTo numbers [ a; b ]
           } -> a.Value * b.Value
         | _ -> 0)
 
-file |> calculateGearRatio
+parsedSample |> calculateGearRatio
+parsedFile |> calculateGearRatio
